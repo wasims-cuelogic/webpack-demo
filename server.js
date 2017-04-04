9c86ad0a596bc872d3d6fe73bd1e0939ff7e6255
Wasim <wasim.sayyed@cuelogic.com>
2017-04-04 15:07:36 +0530

    Adding server file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8001;
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "./build")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./build/index.html"))
+});
+
+app.listen(port);
+console.log('Server started!');
